68ac43e3e59196413176f4e1d501a71e07916f47
Pratik <pratik080.rejoice@gmail.com>
2025-02-06 16:50:49 +0530

    feat: array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,4 +1,31 @@
 // Methods of array
 
-// changes done by sakshi
-// changes done by pratik
+let array = ["Banana", "Orange", "Apple", "Mango", "Apple"]
+let array2 = ['Turiyu', [[['Papaya']]]]
+
+// Basic methods
+// document.write(array)
+// console.log(array[2])
+// console.log(array.join(' : '))
+// console.log(array.pop())
+// console.log(array.pop())
+// console.log(array)
+// console.log(array.push('Kiwi'))
+// console.log(array.push('Guava'))
+// console.log(array)
+// console.log(array.shift())
+// console.log(array)
+// console.log(array.unshift('Watermelon'))
+// console.log(array)
+// // delete array[3]
+// console.log(array.concat(array2, ['test'], ['testt', 'testset']))
+// console.log(array.copyWithin(2, 1))
+// console.log(array2.flat(3))
+// console.log(array.splice(1, 2, 'Strawberry'));
+// console.log(array.toSpliced(1, 2, 'Strawberry'));
+// console.log(array.slice(1, 2))
+
+// Search and find methods
+console.log(array.indexOf('Apple'))
+console.log(array.lastIndexOf('Apple'))
+console.log(array.includes('apple'))

9d5d926a8fa19f1116d63bf05bcc44bc43a579c2
Pratik <pratik080.rejoice@gmail.com>
2025-02-28 16:56:40 +0530

    feat: fetch api

--- fetchAPI.js
+++ fetchAPI.js
@@ -22,7 +22,33 @@ fetch('https://jsonplaceholder.typicode.com/photos').then((res) => {
 // POST method
 fetch('https://jsonplaceholder.typicode.com/photos', {
     method: 'POST',
-    body: {}
+    body: JSON.stringify({ title: 'test' }),
+    headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' }
+}).then((res) => {
+    console.log('res*', res);
+    return res.json()
+}).then((resolvedRes) => {
+    console.log('resolvedRes*', resolvedRes);
+}).catch((error) => {
+    console.log('error**', error);
+})
+
+// PUT/PATCH method
+fetch('https://jsonplaceholder.typicode.com/photos/1', {
+    method: 'PUT',  // PATCH
+    body: JSON.stringify({ title: 'test' })
+}).then((res) => {
+    console.log('res*', res);
+    return res.json()
+}).then((resolvedRes) => {
+    console.log('resolvedRes*', resolvedRes);
+}).catch((error) => {
+    console.log('error**', error);
+})
+
+// DELETE method
+fetch('https://jsonplaceholder.typicode.com/photos/3', {
+    method: 'DELETE'
 }).then((res) => {
     console.log('res*', res);
     return res.json()

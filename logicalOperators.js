b0ef3f258257bd7966ef84705f0962bf047678da
Pratik <pratik080.rejoice@gmail.com>
2025-03-03 16:55:36 +0530

    feat: spread operator

--- logicalOperators.js
+++ logicalOperators.js
@@ -5,3 +5,11 @@ let size = 'xl'
 
 color !== 'blue' && size === 'xl' && document.write('this is printed')
 color === 'blue' || size === 'xl' || document.write('this is printed')
+
+// let baatli = ['d', 'h', 'o', 'o', 'd', 'h']
+// let tapeli = [ ...baatli ]
+
+// let obj = { test: 'test' }
+// let obj2 = { ...obj, test2: 'test2',  }
+
+// console.log(obj2);
